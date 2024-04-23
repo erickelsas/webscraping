066cc3c2fa19a00543448c4f27df9b4a7c561d93
Erick Freitas <erickelsas@alunos.utfpr.edu.br>
2024-04-23 17:01:57 -0300

    build

--- dist/server.mjs
+++ dist/server.mjs
@@ -1,20 +1,12 @@
-var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
-  get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
-}) : x)(function(x) {
-  if (typeof require !== "undefined")
-    return require.apply(this, arguments);
-  throw Error('Dynamic require of "' + x + '" is not supported');
-});
-
 // src/server.ts
 import fastify from "fastify";
 import * as cheerio from "cheerio";
+import axios from "axios";
 var app = fastify();
-var rp = __require("request-promise");
 app.get("/", (req, res) => {
   const url = "http://bianca.com/";
-  rp(url).then((html) => {
-    const $ = cheerio.load(html);
+  axios(url).then((html) => {
+    const $ = cheerio.load(html.data);
     res.status(200).send({ "h1": $("h1").text() });
   }).catch((err) => {
     res.status(500).send("Aconteceu um erro");
